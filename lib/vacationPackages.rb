12ab218f007e44a749328b089615b0eabbc99e6a
Baktiar Miah <baktiar.m@gmail.com>
2019-10-05 23:34:52 +0000

    add scraper webpage

---
+++ lib/vacationPackages.rb
@@ -0,0 +1,10 @@
+require_relative "vacationPackages/version"
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+
+module VacationPackages
+  class Error < StandardError; end
+  
+end
---
+++ lib/vacationPackages/scraper.rb
@@ -0,0 +1,13 @@
+class Scraper
+   def get_page 
+    Nokogiri::HTML(open("https://www.cheapcaribbean.com"))
+   end
+
+ def get_deals
+   resorts = self.get_page.css("#deal_ResortUrl").children
+   destinations = self.get_page.css("#deal_DestinationUrl").children
+   price = doc.css(".pricePoints.extraLarge").first.css(".estPrice").text
+ end 
+
+
+end 
